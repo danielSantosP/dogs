41d7e290deb111ee28db0e9312a2461a580de0bf
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-10-05 15:12:36 -0300

    userContext

--- src/App.js
+++ src/App.js
@@ -9,22 +9,24 @@ import LoginCreate from './Components/Login/LoginCreate';
 import LoginForm from './Components/Login/LoginForm';
 import LoginPasswordLost from './Components/Login/LoginPasswordLost';
 import LoginPasswordReset from './Components/Login/LoginPasswordReset';
-
+import { UserStorage } from './UserContext';
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />}>
-            <Route path="" element={<LoginForm />}/>
-            <Route path="criar" element={<LoginCreate />} />
-            <Route path="perdeu" element={<LoginPasswordLost />} />
-            <Route path="resetar" element={<LoginPasswordReset />} />
-          </Route>
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />}>
+              <Route path="" element={<LoginForm />}/>
+              <Route path="criar" element={<LoginCreate />} />
+              <Route path="perdeu" element={<LoginPasswordLost />} />
+              <Route path="resetar" element={<LoginPasswordReset />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,40 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { TOKEN_POST, USER_GET } from '../../api'
 import useForm from '../../Hooks/useForm'
 import Button from '../Forms/Button'
 import Input from '../Forms/Input'
-
+import {UserContext} from '../../UserContext'
 const LoginForm = () => {
   const username = useForm()
   const password = useForm()
 
-  React.useEffect(()=>{
-    const token = window.localStorage.getItem('token')
-    if(token){
-      getUser(token)
-    }
-  },[])
-
-  const getUser = async (token) => {
-    const {url, options} = USER_GET(token)
-    const response = await fetch(url, options)
-    const json = await response.json()
-    console.log(json)
-  }
+  const {userLogin} = React.useContext(UserContext)
 
   const handleSubmit = async (event) => {
     event.preventDefault()
     if(username.validate() && password.validate()){
-      const {url, options} = TOKEN_POST({
-        username: username.value,
-        password: password.value
-      })
-
-      const response = await fetch(url, options)
-      const json = await response.json()
-      window.localStorage.setItem('token', json.token)
-      getUser(json.token)
+     userLogin(username.value, password.value)
     }
   }
 
