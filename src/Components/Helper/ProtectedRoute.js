fe5dced825b1593ba687bd82dd9fe4c70f4e2362
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-10-06 15:56:05 -0300

    protected route

---
+++ src/Components/Helper/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Navigate } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
+
+const ProtectedRoute = (props) => {
+
+  const {login} = React.useContext(UserContext)
+
+  if(login){
+    return <Route {...props} />
+  } else if (!login){ 
+    return <Navigate to="/login" />
+  } else {
+    return null
+  }
+}
+
+export default ProtectedRoute
