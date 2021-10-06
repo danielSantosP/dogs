36592fcab3587baa63b7aa02806c1ccb47b14265
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-10-06 17:18:10 -0300

    user header

---
+++ src/Components/User/UserHeaderNav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
+import {ReactComponent as MyPhotos} from '../../Assets/feed.svg'
+import {ReactComponent as Stats} from '../../Assets/estatisticas.svg'
+import {ReactComponent as AddPhoto} from '../../Assets/adicionar.svg'
+import {ReactComponent as Logout} from '../../Assets/sair.svg'
+import styles from './UserHeaderNav.module.css'
+
+const UserHeaderNav = () => {
+  const [mobile, setMobile] = React.useState(null)
+  const { userLogout } = React.useContext(UserContext)
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MyPhotos />
+        {mobile && 'Minhas Fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Stats />
+        {mobile && 'Estatísticas'}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AddPhoto />
+        {mobile && 'Adicionar Foto'}
+      </NavLink>
+      <button onClick={userLogout}>
+        <Logout />
+        {mobile && 'Sair'}
+      </button>
+    </nav>
+  )
+}
+
+export default UserHeaderNav
