a744073d1c6a11b344e393339008ad3b0116cd6d
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-10-07 14:13:32 -0300

    feed modal

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './FeedPhotosItem.module.css'
 
-const FeedPhotosItem = ({photo}) => {
+const FeedPhotosItem = ({photo, setModalPhoto}) => {
+  
+  function handleclick(){
+    setModalPhoto(photo)
+  }
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleclick}>
        <img src={photo.src} alt={photo.title} />
        <span className={styles.visualization}>{photo.acessos}</span>
     </li>
